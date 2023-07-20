7c95cb5ccb8bf38c3dc33c73408d92c169ea156e
Oulinstar <terleeva89@gmail.com>
2023-07-20 20:19:33 +0200

    new commit

--- create_product.php
+++ create_product.php
@@ -33,7 +33,7 @@ if (isset($_POST["add"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bags</title>
+    <title>Nice Bags</title>
 </head>
 
 <body>
